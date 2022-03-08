c89c89e8ce956ba21ee167d5505d57283dcbece5
xuthus5 <xuthus5@gmail.com>
2022-03-08 18:42:19 +0800

    fix

--- src/components/upyun_upload/upyun.c
+++ src/components/upyun_upload/upyun.c
@@ -11,14 +11,14 @@ struct _CocoUpYunUpload {
 G_DEFINE_TYPE ( CocoUpYunUpload, coco_upyun_upload, ADW_TYPE_BIN)
 
 static void
-coco_upyun_upload_class_init(CocoClipboardClass *klass) {
+coco_upyun_upload_class_init(CocoUpYunUploadClass *klass) {
     GtkWidgetClass *widget_class = GTK_WIDGET_CLASS(klass);
 
     gtk_widget_class_set_template_from_resource(widget_class, "/cc/xuthus/coco/components/upyun_upload/upyun.ui");
 }
 
 static void
-coco_upyun_upload_init(CocoClipboard *self) {
+coco_upyun_upload_init(CocoUpYunUpload *self) {
     gtk_widget_init_template(GTK_WIDGET(self));
     return;
 }
