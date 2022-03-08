7765d23d2638384d49309eab3ec2fac8b985caa7
xuthus5 <xuthus5@gmail.com>
2022-03-08 18:43:55 +0800

    fix

--- src/components/upyun_upload/upyun.c
+++ src/components/upyun_upload/upyun.c
@@ -4,21 +4,21 @@
 #include <adwaita.h>
 #include "upyun.h"
 
-struct _CocoUpYunUpload {
+struct _CocoUpyunUpload {
     AdwBin parent_instance;
 };
 
-G_DEFINE_TYPE ( CocoUpYunUpload, coco_upyun_upload, ADW_TYPE_BIN)
+G_DEFINE_TYPE ( CocoUpyunUpload, coco_upyun_upload, ADW_TYPE_BIN)
 
 static void
-coco_upyun_upload_class_init(CocoUpYunUploadClass *klass) {
+coco_upyun_upload_class_init(CocoUpyunUploadClass *klass) {
     GtkWidgetClass *widget_class = GTK_WIDGET_CLASS(klass);
 
     gtk_widget_class_set_template_from_resource(widget_class, "/cc/xuthus/coco/components/upyun_upload/upyun.ui");
 }
 
 static void
-coco_upyun_upload_init(CocoUpYunUpload *self) {
+coco_upyun_upload_init(CocoUpyunUpload *self) {
     gtk_widget_init_template(GTK_WIDGET(self));
     return;
 }
--- src/components/upyun_upload/upyun.h
+++ src/components/upyun_upload/upyun.h
@@ -6,6 +6,6 @@ G_BEGIN_DECLS
 
 #define COCO_TYPE_UPYUN_UPLOAD (coco_upyun_upload_get_type())
 
-G_DECLARE_FINAL_TYPE ( CocoUpYunUpload, coco_upyun_upload, COCO, UPYUN_UPLOAD, AdwBin )
+G_DECLARE_FINAL_TYPE ( CocoUpyunUpload, coco_upyun_upload, COCO, UPYUN_UPLOAD, AdwBin )
 
 G_END_DECLS
