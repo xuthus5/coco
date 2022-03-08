592a20a7e6e2729f0db50f0b254de49545ae2cf3
xuthus5 <xuthus5@gmail.com>
2022-03-08 18:32:52 +0800

    fix

---
+++ src/components/upyun_upload/upyun.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include <adwaita.h>
+#include "upyun.h"
+
+struct _CocoUpYunUpload {
+    AdwBin parent_instance;
+};
+
+G_DEFINE_TYPE ( CocoUpYunUpload, coco_upyun_upload, ADW_TYPE_BIN)
+
+static void
+coco_upyun_upload_class_init(CocoClipboardClass *klass) {
+    GtkWidgetClass *widget_class = GTK_WIDGET_CLASS(klass);
+
+    gtk_widget_class_set_template_from_resource(widget_class, "/cc/xuthus/coco/components/upyun_upload/upyun.ui");
+}
+
+static void
+coco_upyun_upload_init(CocoClipboard *self) {
+    gtk_widget_init_template(GTK_WIDGET(self));
+    return;
+}
---
+++ src/components/upyun_upload/upyun.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <adwaita.h>
+
+G_BEGIN_DECLS
+
+#define COCO_TYPE_UPYUNUPLOAD (coco_upyun_upload_get_type())
+
+G_DECLARE_FINAL_TYPE ( CocoUpYunUpload, coco_upyun_upload, COCO, UPYUNUPLOAD, AdwBin )
+
+G_END_DECLS
